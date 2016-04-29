347d59bfa8294691fe9c194f80cff12eeeec0815
huuanh <huuanh90@gmail.com>
2016-04-29 09:44:38 +0700

    nothign much

--- resources/views/admin/adminLayout.blade.php
+++ resources/views/admin/adminLayout.blade.php
@@ -80,8 +80,11 @@
         @if(\Illuminate\Support\Facades\Session::has('flash_message'))
             <div class="alert alert-success"><span class="glyphicon glyphicon-ok"></span><em> {!! session('flash_message') !!}</em></div>
         @endif
+        @if(\Illuminate\Support\Facades\Session::has('flash_warning'))
+                    <div class="alert alert-warning"><span class="glyphicon glyphicon-ok"></span><em> {!! session('flash_message_delete') !!}</em></div>
+                @endif
         @if(\Illuminate\Support\Facades\Session::has('flash_danger'))
-            <div class="alert alert-danger"><span class="glyphicon glyphicon-ok"></span><em> {!! session('flash_message_delete') !!}</em></div>
+            <div class="alert alert-danger"><span class="glyphicon glyphicon-ok"></span><em> {!! session('flash_message') !!}</em></div>
         @endif
 
         @yield('content')
